3b920862418f28922e6bfe6c881ee4735f2757c2
Gia Bao <nguyenhiengiabao12@gmail.com>
2024-08-15 00:35:42 +0700

    Setting an Appbar with Title & Actions

--- lib/pages/expense.dart
+++ lib/pages/expense.dart
@@ -28,6 +28,15 @@ class _ExpenseState extends State<Expense> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           const Text('The chart'),
